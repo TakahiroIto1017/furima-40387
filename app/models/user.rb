c37e08c3871591bcb696ec34cde253bd40c0ca3c
TakahiroIto1017 <tigertrack@gmail.com>
2024-03-18 15:40:16 +0900

    バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
+  validates :password, format: { with: /\A(?=.*[a-z])(?=.*\d)[a-z\d]+\z/i }
+
   validates :nickname, presence: true
   validates :first_name, presence: true, format: { with: /\A[\p{Han}\p{Hiragana}\p{Katakana}]+\z/ }
   validates :last_name, presence: true, format: { with: /\A[\p{Han}\p{Hiragana}\p{Katakana}]+\z/ }
