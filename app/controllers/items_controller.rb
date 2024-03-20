f5a7f74ad269c7336d1ad3339c97129de64f19e8
TakahiroIto1017 <tigertrack@gmail.com>
2024-03-20 18:22:33 +0900

    ストロングパラメーター設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,4 +9,10 @@ class ItemsController < ApplicationController
   def create
   end
 
+  private
+
+  def item_params
+    params.require(:item).permit(:title, :description, :category_id, :condition_id, :shipping_cost_id, :prefecture_id, :shipping_date_id, :price).merge(user_id: current_user.id)
+  end
+
 end
