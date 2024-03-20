50768f3f4e796d8c1a62a0833421c7dd8fa13dc2
TakahiroIto1017 <tigertrack@gmail.com>
2024-03-20 18:15:09 +0900

    バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,20 @@
 class Item < ApplicationRecord
+
   belongs_to :user
   has_one_attached :image
+  
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :condition
   belongs_to :shipping_cost
   belongs_to :prefecture
   belongs_to :shipping_date
+
+  validates :title, presence: true, limit: 40
+  validates :description, presence: true, limit: 1000
+  validates :category_id, :condition_id, :condition_id, :shipping_cost_id, :prefecture_id, :shipping_date_id,
+            presence: true, numericality: { other_than: 0 , message: "can't be blank" }
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+  validates :user, presence: true, foreign_key: true
+
 end
