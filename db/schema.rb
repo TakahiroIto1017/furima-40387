01863be50a9fd3560984787577eadb538b336f48
TakahiroIto1017 <tigertrack@gmail.com>
2024-03-21 14:02:03 +0900

    カラム名修正

--- db/schema.rb
+++ db/schema.rb
@@ -46,7 +46,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_19_123756) do
     t.integer "condition_id", null: false
     t.integer "shipping_cost_id", null: false
     t.integer "prefecture_id", null: false
-    t.integer "hipping_date_id", null: false
+    t.integer "shipping_date_id", null: false
     t.integer "price", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
