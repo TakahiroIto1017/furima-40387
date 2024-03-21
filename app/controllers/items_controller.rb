3c4db5eecf639c69c3006a2639e0d7d02ae2d8e1
TakahiroIto1017 <tigertrack@gmail.com>
2024-03-21 17:04:54 +0900

    自動整形

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,4 @@
 class ItemsController < ApplicationController
-
   before_action :authenticate_user!, only: :new
 
   def index
@@ -21,7 +20,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:title, :description, :category_id, :condition_id, :shipping_cost_id, :prefecture_id, :shipping_date_id, :price, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:title, :description, :category_id, :condition_id, :shipping_cost_id, :prefecture_id,
+                                 :shipping_date_id, :price, :image).merge(user_id: current_user.id)
   end
-
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,7 @@
 class Item < ApplicationRecord
-
   belongs_to :user
   has_one_attached :image
-  
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :condition
@@ -13,8 +12,7 @@ class Item < ApplicationRecord
   validates :title, presence: true, length: { maximum: 40 }
   validates :description, presence: true, length: { maximum: 1000 }
   validates :category_id, :condition_id, :condition_id, :shipping_cost_id, :prefecture_id, :shipping_date_id,
-            presence: true, numericality: { other_than: 0 , message: "can't be blank" }
-  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
-  validates :image, presence:true, blob: { content_type: :image }
-
+            presence: true, numericality: { other_than: 0, message: "can't be blank" }
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  validates :image, presence: true, blob: { content_type: :image }
 end
