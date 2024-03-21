a49e45cd095b521ac61f62ea7c3f5191b6df0c53
TakahiroIto1017 <tigertrack@gmail.com>
2024-03-21 13:47:24 +0900

    バリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,8 +10,8 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :shipping_date
 
-  validates :title, presence: true, limit: 40
-  validates :description, presence: true, limit: 1000
+  validates :title, presence: true, length: { maximum: 40 }
+  validates :description, presence: true, length: { maximum: 1000 }
   validates :category_id, :condition_id, :condition_id, :shipping_cost_id, :prefecture_id, :shipping_date_id,
             presence: true, numericality: { other_than: 0 , message: "can't be blank" }
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
