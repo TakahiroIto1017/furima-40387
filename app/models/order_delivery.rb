89fcd6507586860438eabed8a218e9318167772a
TakahiroIto1017 <tigertrack@gmail.com>
2024-03-27 20:07:51 +0900

    Formオブジェクト修正

--- app/models/order_delivery.rb
+++ app/models/order_delivery.rb
@@ -1,6 +1,6 @@
 class OrderDelivery
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :municipality, :house_number, :building_name, :telephone_number
+  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :municipality, :house_number, :building_name, :telephone_number, :order_id
 
   validates :postal_code, presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
   validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
@@ -10,6 +10,6 @@ class OrderDelivery
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
-    Delivery.create(postal_code: postal_code, prefecture_id: prefecture_id, municipality: municipality, house_number: house_number, building_name: building_name, telephone_number: telephone_number, order_id: order_id)
+    Delivery.create(postal_code: postal_code, prefecture_id: prefecture_id, municipality: municipality, house_number: house_number, building_name: building_name, telephone_number: telephone_number, order_id: order.id)
   end
 end
