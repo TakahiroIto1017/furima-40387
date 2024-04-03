0985d16201334482b89aac1a8fce0aa2db8e355c
TakahiroIto1017 <tigertrack@gmail.com>
2024-04-03 18:14:37 +0900

    レビュー後リファクタリング

--- app/models/order_delivery.rb
+++ app/models/order_delivery.rb
@@ -3,11 +3,16 @@ class OrderDelivery
   attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :municipality, :house_number, :building_name,
                 :telephone_number, :order_id, :token
 
-  validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
-  validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
-  validates :user_id, :item_id, :postal_code, :municipality, :house_number, presence: true
-  validates :telephone_number, presence: true, format: { with: /\A[0-9]{10,11}\z/, message: 'is invalid. Only 10 or 11 numbers' }
-  validates :token, presence: { message: "can't be blank. Enter your credit card information" }
+  with_options presence: true do
+    validates :user_id
+    validates :item_id
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
+    validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+    validates :municipality
+    validates :house_number
+    validates :telephone_number, format: { with: /\A[0-9]{10,11}\z/, message: 'is invalid. Only 10 or 11 numbers' }
+    validates :token, presence: { message: "can't be blank. Enter your credit card information" }
+  end
 
   def save
     order = Order.create(user_id:, item_id:)
